cb72b99a9d127f7e8d09a8cdc80b144e830094c3
DarioLopes <dariolopes2001@gmail.com>
2023-11-10 16:44:29 -0300

    commit de variaveis

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,18 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        String name = "Dário";
+        String name2;
+        name2 = "Brazil";
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b;
+
+        System.out.println("Hello and welcome to "+ name2 + ", " + name);
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
@@ -13,5 +24,6 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+        System.out.println("Soma dos valores= " + soma + "\nSubtração dos valores= " + subtracao + "\nMultiplicação dos valores= " + multiplicacao + "\nDivisão dos valores= " + divisao);
     }
 }
