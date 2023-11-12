580573cd5ef216c67eac18e12f9dedec8937318a
DarioLopes <dariolopes2001@gmail.com>
2023-11-12 01:07:59 -0300

    new file commit for Functions or methods

---
+++ src/Funcao.java
@@ -0,0 +1,19 @@
+public class Funcao {
+    public static void main(String[] args) {
+//        String name = "Dário";
+//        saudacao(name);
+        int resultado = soma(2, 3);
+        System.out.println(resultado);
+        System.out.println(soma(3, 4));
+    }
+//    public static void saudacao(String name)
+//    {
+//        System.out.println("Hello,  " + name + ".");
+//    }
+
+    public static int soma(int x, int y){
+        return x + y;
+    }
+
+
+}
